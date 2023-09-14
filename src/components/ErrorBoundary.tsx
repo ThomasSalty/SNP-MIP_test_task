6f9d141db7ea6271e3cb4651cbcb68ca7c2def04
Tamas Soos <Tamas_Soos@epam.com>
2023-09-14 10:59:59 +0200

    Add ErrorBoundary component

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,31 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+type ErrorBoundaryProps = {
+	children: ReactNode;
+};
+
+type ErrorBoundaryState = {
+	hasError: boolean;
+};
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+	constructor(props: ErrorBoundaryProps) {
+		super(props);
+		this.state = { hasError: false };
+	}
+
+	componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+		console.error(error, errorInfo);
+		this.setState({ hasError: true });
+	}
+
+	render() {
+		if (this.state.hasError) {
+			return <div>Something went wrong. Check the console for details!</div>;
+		}
+
+		return this.props.children;
+	}
+}
+
+export default ErrorBoundary;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "../styles/index.scss";
 import { Provider, store } from "./state.ts";
+import ErrorBoundary from "./components/ErrorBoundary.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<ErrorBoundary>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</ErrorBoundary>
 	</React.StrictMode>
 );
